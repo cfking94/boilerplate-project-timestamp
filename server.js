ad7bd0b8856926e7d4e37975712e0754b671b69f
cfking94 <72390055+cfking94@users.noreply.github.com>
2021-05-08 14:56:18 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,59 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint... 
+app.get("/api", function(req, res) {
+  return res.json({
+    unix: Date.now(),
+    utc: new Date().toUTCString()
+  });
+});
+
+app.get("/api/:date", function (req, res) {
+  var date = req.params.date;
+
+  // Test date in unix
+  if (/\d{5,}/.test(date)) {
+    var dateInt = parseInt(date);
+
+    return res.json({
+      unix: dateInt,
+      utc: new Date(dateInt).toUTCString()
+    });
+  }
+  // Date in ISO-8601
+  else {
+    if (new Date(date) == "Invalid Date") {
+      return res.json({
+        error: "Invalid Date"
+      });
+    } else {
+      res.json({
+        unix: new Date(date).getTime(),
+        utc: new Date(date).toUTCString()
+      });
+    }
+  }
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
